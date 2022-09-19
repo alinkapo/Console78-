a79209ac4718f808486e59f7b558c2a3d0f078ae
Андрей <@DESKTOP-USER>
2022-09-19 17:57:40 +0400

    Добавьте файлы проекта.

---
+++ Console78 +/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Console78
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string a in myReturn())
+            Console.WriteLine(a);
+
+            int[] age = {18};
+           
+           foreach (int z in age)
+                Console.WriteLine(z);
+        }
+         
+       static Array myReturn()
+        {
+           foreach (string b in Return())
+                Console.WriteLine(b);
+            string[] city = {"Samara"};
+            return city;
+        }
+        
+        static Array Return()
+        {
+            string[] fio = {"Положай Алина Андреевна"};
+            return fio;
+        }
+    }
+}
